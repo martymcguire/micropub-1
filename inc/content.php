dc1dd2179807ec1aaee59ee50d4d2541e11ae31b
Marty McGuire <schmartissimo@gmail.com>
2018-12-30 00:52:23 -0500

    updates for martymcgui.re. some general cleanups.
    * allow source path and URL path to differ
        * source paths have a hugo post type prefix and urls do not
    * allow dates from client
        * required a change in path and URL config handling.
    * allow source for a given URL to check multiple locations
        * reverse-map config for post types to content types
        * allow dashes instead of slashes in date portions
        * allow .md and .html file extensions
        * accept the first found file and 404 if none exist.
    * more general mapping of mf2 props to hugo props
        * map on the way in for create, update
        * unmap on the way out for source requests
    * preserve audio, video, and category array props from flattening on normalize
    * drop posttype_source_function since my setup handles reply contexts downstream
    * stop creating _index.md files since my setup disallows hugo's default indexing.
    * clean up array keys when deleting from array properties like categories (tags)
    * content can be in properties as {'content': [{'html':'blah blah'}]} (quill articles)
    * post type discovery for h-entry only. others get the h- value. e.g. h-event => event.
    * allow config of extra per-post properties by post type.
    * normalize event start and end times (grr hugo date parsing) and use start as post date on create.

--- inc/content.php
+++ inc/content.php
@@ -2,27 +2,48 @@
 
 use Symfony\Component\Yaml\Yaml;
 
+// my permalinks are /YYYY/MM/DD/SLUG/
+// but these map to, typically, {source_path}/content/posts/
+// and failing that, {source_path}/content/events/
+// this relies on HAXX, puling the before-the-date prefix info
+// from config.php's `content_paths`.
 function get_source_from_url($url) {
     global $config;
 
     # our config has the Hugo root, so append "content/".
     $source_path = $config['source_path'] . 'content/';
-    $path = str_replace($config['base_url'], $source_path, $url);
-    if ('index.html' == substr($path, -10)) {
-        # if this was a full URL to "/index.html", replace that with ".md"
-        $path = str_replace('/index.html', '.md', $path);
-    } elseif ( '.html' == substr($path, -5)) {
-        # if this is a URL ending in .html but not index.html, replace with ".md"
-        $path = str_replace('.html', '.md', $path);
-    } elseif ( '/' == substr($path, -1)) {
-        # if this is a URL ending in just "/", replace that with ".md"
-        $path = rtrim($path, '/') . '.md';
-    } else {
-        # should be a URL of the directory containing index.htm, so just
-        # tack on ".md" to the path
-        $path .= '.md';
+    $possible_prefixes = array_values(array_unique(array_map("array_shift", array_values($config['content_paths']))));
+    $possible_paths = [];
+    $nested_path = str_replace($config['base_url'], '', $url);
+    $flat_path = rtrim(str_replace("/", "-", $nested_path), '-'); # older content is content/post/YYYY-MM-DD-hhmmss.md
+    foreach([$nested_path, $flat_path] as $unprefixed_path) {
+        foreach($possible_prefixes as $prefix) {
+            $path = $source_path . $prefix . $unprefixed_path;
+            if ('index.html' == substr($path, -10)) {
+                # if this was a full URL to "/index.html", replace that with ".md"
+                $path = str_replace('/index.html', '.md', $path);
+            } elseif ( '.html' == substr($path, -5)) {
+                # if this is a URL ending in .html but not index.html, replace with ".md"
+                $path = str_replace('.html', '.md', $path);
+            } elseif ( '/' == substr($path, -1)) {
+                # if this is a URL ending in just "/", replace that with ".md"
+                $path = rtrim($path, '/') . '.md';
+            } else {
+                # should be a URL of the directory containing index.htm, so just
+                # tack on ".md" to the path
+                $path .= '.md';
+            }
+            array_push($possible_paths, $path);
+            array_push($possible_paths, rtrim($path, 'md') . 'html'); # some files are .html, don't hate.
+        }
     }
-    return $path;
+    foreach($possible_paths as $path) {
+        if( file_exists($path) ) {
+            return $path;
+        }
+    }
+    header('HTTP/1.1 404 Not Found');
+    die();
 }
 
 function parse_file($original) {
@@ -45,7 +66,7 @@ function parse_file($original) {
 # this function fetches the source of a post and returns a JSON
 # encoded object of it.
 function show_content_source($url, $properties = []) {
-    $source = parse_file( get_source_from_url($url) );
+    $source = unmap_properties( parse_file( get_source_from_url($url) ) );
     $props = [];
 
     # the request may define specific properties to return, so
@@ -57,7 +78,7 @@ function show_content_source($url, $properties = []) {
             }
         }
     } else {
-        $props = parse_file( get_source_from_url($url) );
+        $props = $source;
     }
     header( "Content-Type: application/json");
     print json_encode( [ 'properties' => $props ] );
@@ -73,45 +94,53 @@ function slugify($string) {
 
 # this takes an MF2 array of arrays and converts single-element arrays
 # into non-arrays.
+# TODO: i prefer most things to stay arrays! jf2 might have the exceptions
+# that i'm interested in singularizing.
 function normalize_properties($properties) {
     $props = [];
+    $array_props = ['audio','photo','video','category'];
     foreach ($properties as $k => $v) {
-        # we want the "photo" property to be an array, even if it's a
+        # we want certain properties to be an array, even if it's a
         # single element.  Our Hugo templates require this.
-        if ($k == 'photo') {
+        if (in_array($k, $array_props)) {
             $props[$k] = $v;
         } elseif (is_array($v) && count($v) === 1 && (!is_array($v[0]))) {
-	    # flatten properties *unless* they contain nested objects.
+            # flatten properties *unless* they contain nested objects.
             $props[$k] = $v[0];
         } else {
             $props[$k] = $v;
         }
     }
-    # MF2 defines "name" instead of title, but Hugo wants "title".
-    # Only assign a title if the post has a name.
-    if (isset($props['name'])) {
-        $props['title'] = $props['name'];
-    }
+
+    $props = map_properties($props);
     return $props;
 }
 
-# this function is a router to other functions that can operate on the source
-# URLs of reposts, replies, bookmarks, etc.
-# $type = the indieweb type (https://indieweb.org/post-type-discovery)
-# $properties = array of front-matter properties for this post
-# $content = the content of this post (which may be an empty string)
-#
-function posttype_source_function($posttype, $properties, $content) {
-    # replace all hyphens with underscores, for later use
-    $type = str_replace('-', '_', $posttype);
-    # get the domain of the site to which we are replying, and convert
-    # all dots to underscores.
-    $target = str_replace('.', '_', parse_url($properties[$posttype], PHP_URL_HOST));
-    # if a function exists for this type + target combo, call it
-    if (function_exists("${type}_${target}")) {
-        list($properties, $content) = call_user_func("${type}_${target}", $properties, $content);
+# MF2 defines properties that we'd rather map to other names in Hugo.
+# Additionally, when we respond to a micropub source request, we need to unmap them first.
+$properties_to_map = array (
+    'name' => 'title',
+    'category' => 'tags'
+);
+function map_properties($props) {
+    global $properties_to_map;
+    foreach($properties_to_map as $mfprop => $hugoprop) {
+        if (isset($props[$mfprop])) {
+            $props[$hugoprop] = $props[$mfprop];
+            unset($props[$mfprop]);
+        }
     }
-    return [$properties, $content];
+    return $props;
+}
+function unmap_properties($props) {
+    global $properties_to_map;
+    foreach($properties_to_map as $mfprop => $hugoprop) {
+        if (isset($props[$hugoprop])) {
+            $props[$mfprop] = $props[$hugoprop];
+            unset($props[$hugoprop]);
+        }
+    }
+    return $props;
 }
 
 # this function accepts the properties of a post and
@@ -143,7 +172,7 @@ function post_type_discovery($properties) {
 }
 
 # given an array of front matter and body content, return a full post
-function build_post( $front_matter, $content) {
+function build_post( $front_matter, $content ) {
     ksort($front_matter);
     return "---\n" . Yaml::dump($front_matter) . "---\n" . $content . "\n";
 }
@@ -155,8 +184,8 @@ function write_file($file, $content, $overwrite = false) {
         if ( FALSE === mkdir(dirname($file), 0777, true) ) {
             quit(400, 'cannot_mkdir', 'The content directory could not be created.');
         }
-        # create an _index.md file so that Hugo can make a browseable dir
-        touch(dirname($file) . '/_index.md');
+        # NOTE: i don't need these ! create an _index.md file so that Hugo can make a browseable dir
+        // touch(dirname($file) . '/_index.md');
     }
     if (file_exists($file) && ($overwrite == false) ) {
         quit(400, 'file_conflict', 'The specified file exists');
@@ -193,7 +222,7 @@ function undelete($request) {
 
 function update($request) {
     $filename = get_source_from_url($request->url);
-    $original = parse_file($filename);
+    $original = unmap_properties(parse_file($filename));
     foreach($request->update['replace'] as $key=>$value) {
         $original[$key] = $value;
     }
@@ -214,7 +243,7 @@ function update($request) {
             }
         } else {
             # deleting one or more elements from a property
-            $original[$key] = array_diff($original[$key], $value);
+            $original[$key] = array_values(array_diff($original[$key], $value));
         }
     }
     $content = $original['content'][0];
@@ -232,11 +261,14 @@ function create($request, $photos = []) {
     $properties = normalize_properties($mf2['properties']);
 
     # pull out just the content, so that $properties can be front matter
-    # NOTE: content may be in ['content'] or ['content']['html'].
+    # NOTE: content may be in ['content'] or ['content']['html']
+    # or ['content'][0]['html']!
     # NOTE 2: there may be NO content!
     if (isset($properties['content'])) {
         if (is_array($properties['content']) && isset($properties['content']['html'])) {
             $content = $properties['content']['html'];
+        } elseif (is_array($properties['content']) && isset($properties['content'][0]) && isset($properties['content'][0]['html'])) {
+            $content = $properties['content'][0]['html'];
         } else {
             $content = $properties['content'];
         }
@@ -246,6 +278,7 @@ function create($request, $photos = []) {
     # ensure that the properties array doesn't contain 'content'
     unset($properties['content']);
 
+    // FIXME: other uploads like audio, video.
     if (!empty($photos)) {
         # add uploaded photos to the front matter.
         if (!isset($properties['photo'])) {
@@ -256,15 +289,27 @@ function create($request, $photos = []) {
     }
 
     # figure out what kind of post this is.
-    $properties['posttype'] = post_type_discovery($properties);
+    $mf2_type = $mf2['type'][0];
+    if($mf2_type !== "h-entry") {
+        $posttype = preg_replace("/^h-/", '', $mf2_type);
+    } else {
+        $posttype = post_type_discovery($properties);
+    }
 
-    # invoke any source-specific functions for this post type.
-    # articles, notes, and photos don't really have "sources", other than
-    # their own content.
-    # replies, reposts, likes, bookmarks, etc, should reference source URLs
-    # and may interact with those sources here.
-    if (! in_array($properties['posttype'], ['article', 'note', 'photo', 'ate', 'drank'])) {
-        list($properties, $content) = posttype_source_function($properties['posttype'], $properties, $content);
+    # types can have extra metadata
+    if (isset($config['content_defaults']) && isset($config['content_defaults'][$posttype])) {
+        $properties = array_merge($config['content_defaults'][$posttype], $properties);
+    }
+
+    # normalize event start / end. use start as post date.
+    if(($posttype == 'event') && (isset($properties['start']))) {
+        # make sure start and end are properly formatted
+        # and set date = to start
+        $properties['start'] = (new DateTime($properties['start']))->format('Y-m-d H:i:s O');
+        $properties['date'] = $properties['start'];
+        if(isset($properties['end'])) {
+            $properties['end'] = (new DateTime($properties['end']))->format('Y-m-d H:i:s O');
+        }
     }
 
     # all items need a date
@@ -297,15 +342,14 @@ function create($request, $photos = []) {
     $ts = strtotime($properties['date']);
 
     # we need either a title, or a slug.
-    # NOTE: MF2 defines "name" as the title value.
-    if (!isset($properties['name']) && !isset($properties['slug'])) {
+    if (!isset($properties['title']) && !isset($properties['slug'])) {
         # We will assign this a slug.
         $properties['slug'] = date('His', $ts);
     }
 
     # if we have a title but not a slug, generate a slug
-    if (isset($properties['name']) && !isset($properties['slug'])) {
-        $properties['slug'] = $properties['name'];
+    if (isset($properties['title']) && !isset($properties['slug'])) {
+        $properties['slug'] = $properties['title'];
     }
     # make sure the slugs are safe.
     if (isset($properties['slug'])) {
@@ -320,16 +364,18 @@ function create($request, $photos = []) {
     $url = $config['base_url'];
     # does this type of content require a specific path?
     # 2018-11-19 HAXX: expecting an array with [prefix, date string].
-    if (array_key_exists($properties['posttype'], $config['content_paths'])) {
-	$path_extra_parts = $config['content_paths'][$properties['posttype']];
-	$path_extra = $path_extra_parts[0] . date($path_extra_parts[1], $ts);
-	$path .= $path_extra;
-	$url .= $path_extra;
+    if (array_key_exists($posttype, $config['content_paths'])) {
+        $path_extra_parts = $config['content_paths'][$posttype];
+        $path_date_part = date($path_extra_parts[1], $ts);
+        $path_extra = $path_extra_parts[0] . $path_date_part;
+        $path .= $path_extra;
+        # 2018-12-29 HAXX: i only use /YYYY/MM/DD/SLUG/ permalinks soooo
+        $url .= $path_date_part;
     }
     $filename = $path . $properties['slug'] . '.md';
     /* this differs depending on whether ugly URLs are enabled */
-    //$url .= $properties['slug'] . '/index.html';
-    $url .= $properties['slug'] . '.html';
+    $url .= $properties['slug'] . '/';
+    //$url .= $properties['slug'] . '.html';
 
     # write_file will default to NOT overwriting existing files,
     # so we don't need to check that here.
@@ -364,6 +410,7 @@ function create($request, $photos = []) {
             #       links.  But it's easy enough to collect them, for now.
             $file_contents = build_post($properties, $content);
             write_file($filename, $file_contents, true);
+            # FIXME: gonna need to "rebuild the site" to have these stick meaningfully
         }
     }
     # send a 201 response, with the URL of this item.
